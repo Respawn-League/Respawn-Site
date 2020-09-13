25c70b81a8607af0f84389338be497280ec0a8d3
coretf <connororeilly@outlook.com.au>
2020-09-13 05:39:27 -0700

    Commented out reversible in AddPlacementsToLeagueRosters

--- db/migrate/20181105081426_add_placements_to_league_rosters.rb
+++ db/migrate/20181105081426_add_placements_to_league_rosters.rb
@@ -2,14 +2,14 @@ class AddPlacementsToLeagueRosters < ActiveRecord::Migration[5.2]
   def change
     add_column :league_rosters, :placement, :integer
 
-    reversible do |dir|
-      dir.up do
-        League.find_each do |league|
-          league.divisions.each do |division|
-            Leagues::Rosters::ScoreUpdatingService.call(league, division)
-          end
-        end
-      end
-    end
+#    reversible do |dir|
+#      dir.up do
+#        League.find_each do |league|
+#          league.divisions.each do |division|
+#            Leagues::Rosters::ScoreUpdatingService.call(league, division)
+#          end
+#        end
+#      end
+#    end
   end
 end
